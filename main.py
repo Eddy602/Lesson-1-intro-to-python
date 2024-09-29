015b16f40514ae254a0250fcec319e775660d736
Eddy602 <edwardodjidjaaa@gmail.com>
2024-09-29 15:52:32 +0300

    Python

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello")
+name= "Edward"
+age= 13
+print(name,age)
+print("my name is",name,"my age is",age)
+import keyword
+print(keyword.kwlist)
+a=15
+b=25
+c=a+b
+print(a+b)
+print("the sum of 2 numbers is",c)
